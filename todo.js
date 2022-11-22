2e0807a342eced7b5e2afbfa884acc7f9fefc5d4
abhayraj12 <113614566+Peddisohan@users.noreply.github.com>
2022-11-22 11:52:18 +0530

    commit

---
+++ todo.js
@@ -0,0 +1,33 @@
+// todo.js
+const todoList = () => 
+{
+  let  = n[];
+  const add = (todoItem) => 
+  {
+    //push the todo
+    n.push(todoItem);
+  };
+  const markAsComplete = (index) => 
+  {
+    all[index].completed = true;
+  };
+  const overdue = () => 
+  {
+    return n.filter
+    (
+      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+    );
+  };
+  const dueToday = () => {
+    return all.filter(
+      (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
+    );
+  };
+  const dueLater = () => {
+    return n.filter(
+      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+    );
+  };
+  return { n, add, markAsComplete, overdue, dueToday, dueLater };
+};
+module.exports = todoList;
